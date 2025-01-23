4f7248dc636c3b2e64c33771ccbd5cc2fd55d5c9
MohamadCS <mohamadessa443@gmail.com>
2025-01-23 21:43:16 +0200

    Updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "LoadBalancer.hpp"
 #include "Server.hpp"
 #include "Utils.hpp"
+#include <cmath>
 #include <cstddef>
 #include <cstdlib>
 #include <iostream>
@@ -61,15 +62,18 @@ std::pair<LoadBalancer, double> parseArguments(int argc, char* argv[]) {
         servingRateVec.push_back(std::stod(argv[i]));
     }
 
-    if(probVec.size() != serversNum || maxQueueSizesVec.size() != serversNum || maxQueueSizesVec.size() != serversNum){
+    double probSum = 0;
+    for(auto prob : probVec) {
+        probSum += prob;
+    }
+
+    if (probVec.size() != serversNum || maxQueueSizesVec.size() != serversNum ||
+        maxQueueSizesVec.size() != serversNum || probSum != 1) {
         std::cerr << "Wrong parameters\n";
     }
 
     LoadBalancer loadBalancer{internetSendingRate, std::move(probVec), std::move(maxQueueSizesVec),
                               std::move(servingRateVec)};
-
-
-
 
     return {loadBalancer, simTime};
 }
@@ -79,12 +83,26 @@ std::pair<LoadBalancer, double> parseArguments(int argc, char* argv[]) {
 int main() {
     Server server(1, 2, 1000);
 
-    
+    std::vector<int> vec;
+    for(int i = 10000; i< 100000 ;i+=1000){
+        vec.push_back(i);
+    }
 
-    for (int i = 0; i < 5; ++i) {
-        Server::SimResult result = server.simulate(5);
-        std::cout << "Served "<< result.totalServedQueries <<" Average wait time " <<
-                                 result.waitTime/result.totalServedQueries) << '\n';
+    for (int simTime : vec) {
+        double averageWaitTime = 0;
+        double serviceError = 0;
+        double waitTimeError = 0;
+
+        for (int i = 0; i < 20; ++i) {
+            Server::SimResult result = server.simulate(simTime);
+             averageWaitTime = result.waitTime / result.totalServedQueries;
+            // std::cout << "Served " << result.totalServedQueries << " Average wait time " << averageWaitTime << '\n';
+
+             serviceError += double(std::abs(simTime - double(result.waitTime)) * 100) / simTime;
+             waitTimeError += std::abs(1 - averageWaitTime) * 100;
+
+        }
+        std::cout << "Service Error " << serviceError/20 << ", waitTime Error " << waitTimeError/20 << '\n';
     }
 
     return 0;
